6f571c9d2ea2a7050c42d6e360fb45c35cea74b5
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-11 22:23:44 +0200

    change for jsx

--- components/Contact.js
+++ components/Contact.js
@@ -1,21 +1,30 @@
 var Contact = React.createClass({
-    propTypes: {
-      item: React.PropTypes.object.isRequired,
-    },
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
   render: function() {
-      return (
-        React.createElement('div', {className: 'contactItem'},
+    return (
+/*      React.createElement('div', {className: 'contactItem'},
           React.createElement('img', {
-            className: 'contactImage',
-            src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-          }),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
+              className: 'contactImage',
+              src: 'https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg'
+            }),
+            React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+            React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+            React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+              this.props.item.email
         )
-      )
-    },
-  });
+            )
+    */
+      <div className={'contactItem'}>
+        <img className={contactImage} src={'https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg'}></img>
+        <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
+        <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
+        <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>this.props.item.email</a>
+      </div>
+    )
+  },
+});
+
   
